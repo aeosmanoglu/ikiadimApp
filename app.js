70e8e603e4a74c4407295c33e6ad460d151b2d19
ABUZER EMRE OSMANOGLU <a.osmanoglu@jandarma.gov.tr>
2021-11-08 17:46:04 +0300

    passport added

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 
 const { authenticator } = require("@otplib/preset-default"); // for Secret Key generation
 const qr = require("qrcode"); // for QR Code generation
-const ad2 = require('activedirectory2'); // for active directory login over LDAP
+
+/* PASSPORT */
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const CustomStrategy = require('passport-custom').Strategy
+const { authenticate } = require('ldap-authentication')
 
 /* ENVIROMENT*/
 const dotenv = require("dotenv");
@@ -12,52 +17,83 @@ dotenv.config();
 
 /* Local variables and functions */
 var isQRGenerated = false;
+var key = authenticator.generateSecret();
 
 // TODO: Unit test this func with Jest
 function isValidID(id) {
-    regex = new RegExp(/^\d{7}$/);
+    regex = new RegExp(/[1-9][0-9]{6}/);
     return regex.test(id);
 }
 
+passport.use('ldap', new CustomStrategy(
+    async function(req, done) {
+        try {
+            if (!req.body.id || !req.body.password) {
+                throw new Error('username and password are not provided')
+            }
+
+            let options = {
+                ldapOpts: {
+                    url: process.env.LDAP_URL
+                },
+                adminDn: process.env.ADMIN_DN,
+                adminPassword: process.env.ADMIN_PASSWORD,
+                userDn: "CN=" + req.body.id + process.env.USER_DN,
+                userPassword: req.body.password,
+                userSearchBase: process.env.USER_SEARCH_BASE,
+                usernameAttribute: process.env.USERNAME_ATTR,
+                username: req.body.id,
+            }
+            let user = await authenticate(options)
+            done(null, user)
+        } catch (error) {
+            done(error, null)
+        }
+    }
+))
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+})
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+})
+
+var sessionMiddleWare = cookieSession({
+    name: 'session',
+    keys: key,
+    maxAge: 60 * 60 * 1000 // 1 hour
+})
+
 /* End of requirements */
 
-
 app.use(express.urlencoded({ extended: true }));
+app.use(sessionMiddleWare);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static("public"));
 app.set("view engine", "pug");
 
-app.post(
-    "/",
+app.post('/login',
+    passport.authenticate('ldap', { failureRedirect: '/' }),
     function(req, res) {
-        const config = {
-            url: process.env.LDAP_URL,
-            baseDN: process.env.LDAP_BASE_DN,
-            username: req.body.id,
-            password: req.body.password,
-        }
-        const ad = new ad2(config);
-
-        ad.authenticate(req.body.id, req.body.password, function(err, auth) {
-            if (err) {
-                console.log('ERROR: ' + JSON.stringify(err));
-            }
-
-            if (auth) {
-                console.log('Authenticated!');
-                res.render("index");
-            } else {
-                console.log('Authentication failed!');
-                res.render("login", { message: "Kullanıcı adı veya parola hatalı" });
-
-            }
-        });
+        res.redirect('/success');
     }
-);
+)
+
+app.get('/success', (req, res) => {
+    if (req.sessionCookies.keys === key) {
+        res.render('index')
+    } else {
+        res.redirect('/')
+    }
+})
 
 // LOGOUT
-app.get("/logout", (req, res) => {
-    res.redirect("/");
-});
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+})
 
 // LOGIN
 app.get("/", function(req, res) {
