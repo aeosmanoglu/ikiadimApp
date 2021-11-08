46dde7f1a8045ea547e40f48b95d47fa6a6dd42a
ABUZER EMRE OSMANOGLU <a.osmanoglu@jandarma.gov.tr>
2021-11-08 14:30:35 +0300

    passport removed, ad2 added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+/* EXPRESS */
 const express = require("express");
 const app = express();
-const { authenticator } = require("@otplib/preset-default");
-const qr = require("qrcode");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const CustomStrategy = require("passport-custom").Strategy;
-const { authenticate } = require("ldap-authentication");
+
+const { authenticator } = require("@otplib/preset-default"); // for Secret Key generation
+const qr = require("qrcode"); // for QR Code generation
+const ad2 = require('activedirectory2'); // for active directory login over LDAP
+
+/* ENVIROMENT*/
 const dotenv = require("dotenv");
 dotenv.config();
+
+/* Local variables and functions */
 var isQRGenerated = false;
 
 // TODO: Unit test this func with Jest
@@ -16,99 +19,48 @@ function isValidID(id) {
     return regex.test(id);
 }
 
-// Create the passport custom stragegy and name it `ldap`
-passport.use(
-    "ldap",
-    new CustomStrategy(async function (req, done) {
-        try {
-            if (!req.body.id || !req.body.password || !isValidID(req.body.id)) {
-                throw new Error(
-                    "username and password are not provided or valid"
-                );
-            }
-            // construct the parameter to pass in authenticate() function
-            let ldapBaseDn = process.env.LDAP_BASE_DN;
-            let options = {
-                ldapOpts: {
-                    url: process.env.LDAP_URL,
-                },
-                // note in this example it only use the user to directly
-                // bind to the LDAP server. You can also use an admin
-                // here. See the document of ldap-authentication.
-                userDn: `uid=${req.body.username},${ldapBaseDn}`,
-                userPassword: req.body.password,
-                userSearchBase: ldapBaseDn,
-                usernameAttribute: "uid",
-                username: req.body.id,
-            };
-            // ldap authenticate the user
-            let user = await authenticate(options);
-            // success
-            done(null, user);
-        } catch (error) {
-            // authentication failure
-            done(error, null);
-        }
-    })
-);
+/* End of requirements */
 
-// passport requires
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
-passport.deserializeUser(function (user, done) {
-    done(null, user);
-});
 
-// passport requires a session
-var sessionMiddleWare = cookieSession({
-    name: "session",
-    keys: [authenticator.generateSecret()],
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-});
-
-// The order of the following middleware is very important for passport!!
 app.use(express.urlencoded({ extended: true }));
-app.use(sessionMiddleWare);
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static("public"));
-
 app.set("view engine", "pug");
 
-// user post username and password
 app.post(
-    "/login",
-    passport.authenticate("ldap", {
-        failureRedirect:
-            ("/login", { message: "Kullanıcı Adı veya Parola Hatalı" }),
-    }),
-    function (req, res) {
-        res.redirect("/success");
+    "/",
+    function(req, res) {
+        const config = {
+            url: process.env.LDAP_URL,
+            baseDN: process.env.LDAP_BASE_DN,
+            username: req.body.id,
+            password: req.body.password,
+        }
+        const ad = new ad2(config);
+
+        ad.authenticate(req.body.id, req.body.password, function(err, auth) {
+            if (err) {
+                console.log('ERROR: ' + JSON.stringify(err));
+            }
+
+            if (auth) {
+                console.log('Authenticated!');
+                res.render("index");
+            } else {
+                console.log('Authentication failed!');
+                res.render("login", { message: "Kullanıcı adı veya parola hatalı" });
+
+            }
+        });
     }
 );
 
-// success page
-app.get("/success", (req, res) => {
-    let user = req.user;
-    if (!user) {
-        res.redirect("/");
-        return;
-    }
-    res.render("success", {
-        userDisplayName: user.cn,
-        userObject: JSON.stringify(user, null, 2),
-    });
-});
-
-// passport standard logout call.
+// LOGOUT
 app.get("/logout", (req, res) => {
-    req.logout();
     res.redirect("/");
 });
 
-// the login page
-app.get("/", function (req, res) {
+// LOGIN
+app.get("/", function(req, res) {
     res.render("login");
 });
 
