1a04b142a4b825af399f4da68b6774983a571a4b
ABUZER EMRE OSMANOGLU <a.osmanoglu@jandarma.gov.tr>
2021-11-09 13:07:05 +0300

    log added

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ const { authenticate } = require("ldap-authentication");
 const dotenv = require("dotenv");
 dotenv.config();
 var isQRGenerated = false;
-let key = authenticator.generateSecret();
+var user = {};
 
 // TODO: Unit test this func with Jest
 function isValidID(id) {
     regex = new RegExp(/[1-9][0-9]{6}/);
     return regex.test(id);
+}
+
+function now() {
+    let timeElapsed = Date.now();
+    let today = new Date(timeElapsed);
+    return today.toString();
 }
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,9 +30,14 @@ app.get("/", function(req, res) {
     res.render("login");
 });
 
-app.post('/', async function(res, req) {
-    var user = null;
+app.post('/', async(res, req) => {
     let b = res.body;
+    if (!isValidID(b.id)) {
+        req.render("login", { message: "what the hack are you doing?" });
+        const ip = res.headers['x-forwarded-for'] || res.socket.remoteAddress;
+        console.log("HACK  : " + ip + " - " + now());
+        return;
+    }
     let options = {
         ldapOpts: { url: process.env.LDAP_URL },
         adminDn: process.env.ADMIN_DN,
@@ -42,42 +53,42 @@ app.post('/', async function(res, req) {
         user = await authenticate(options);
 
     } catch (error) {
-        console.log(error);
+        console.debug(error);
+        req.render("login", { message: "Kullanıcı adı veya parola hatalı." });
+        return;
     }
 
-    if (user === null) {
+    if (user === {}) {
         req.render("login", { message: "Kullanıcı adı veya parola hatalı." });
     } else {
-        let name = user.displayName.split(" ");
-        let shortName = name.shift() + " " + name.pop().charAt(0) + ".";
-        req.render('index', { name: shortName });
+        let name = user.givenName;
+        let surname = user.sn.substring(0, 1) + ".";
+        req.render('index', { name: name + " " + surname });
+        console.log("LOGIN : " + user.name + " - " + now());
     }
 });
 
+app.get("/logout", (res, req) => {
+    req.render("login", { message: "Çıkış yapıldı" });
+    console.log("LOGOUT: " + user.name + " - " + now());
+    user = {};
+});
 
-//////////////////////////////////////////////////////////////////////////
-
-// app.get("/home", function (req, res) {
-//     res.render("index", { isQRGenerated });
-// });
-
-// app.post("/home", function bodyParser(req, res) {
-//     const secret = authenticator.generateSecret();
-//     qr.toDataURL(
-//         "otpauth://totp/Jandarma?secret=" + secret,
-//         {
-//             errorCorrectionLevel: "H",
-//             width: 500,
-//             margin: 0,
-//             color: { light: "#FFFFFF00" },
-//         },
-//         (err, src) => {
-//             isQRGenerated = true;
-//             res.render("index", { src, isQRGenerated });
-//         }
-//     );
-// });
-
-// TODO: redirect to other views to "/""
+app.post("/create", (req, res) => {
+    let secret = authenticator.generateSecret();
+    qr.toDataURL(
+        "otpauth://totp/Jandarma?secret=" + secret, {
+            errorCorrectionLevel: "H",
+            width: 500,
+            margin: 0,
+            color: { light: "#FFFFFF00" },
+        },
+        (err, src) => {
+            isQRGenerated = true;
+            res.render("index", { src, isQRGenerated });
+        }
+    );
+    console.log("QR GEN: " + user.name + " - " + now());
+});
 
 module.exports = app;
