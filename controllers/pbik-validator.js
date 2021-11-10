c3b0c2821c29e5ff6dae5e3b66ca5267f32a9fc0
ABUZER EMRE OSMANOGLU <a.osmanoglu@jandarma.gov.tr>
2021-11-10 09:21:12 +0300

    ID can not greater than 7

--- controllers/pbik-validator.js
+++ controllers/pbik-validator.js
@@ -1,6 +1,9 @@
 /*jshint esversion: 8 */
 
 const isValidID = (id = String) => {
+    if (id.length > 7) {
+        return false;
+    }
     regex = new RegExp(/[1-9][0-9]{6}/);
     return regex.test(id);
 };
